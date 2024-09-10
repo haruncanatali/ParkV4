2d09b9d495e7860f633a00266fe92286e602926d
Haruncan Atalı <harun.atali@adayazilim.com>
2024-09-10 18:09:30 +0300

    last

---
+++ ParkV4.Application/Locations/Queries/GetLocation/GetLocationVm.cs
@@ -0,0 +1,8 @@
+using ParkV4.Application.Locations.Queries.Dtos;
+
+namespace ParkV4.Application.Locations.Queries.GetLocation;
+
+public class GetLocationVm
+{
+    public LocationDto Location { get; set; }
+}
